5329b97675608d38030eee1d04d75bfe720b695d
christian.guegi <christian.guegi@memonews.com>
2013-01-11 17:19:15 +0100

    added deletion use case, refactoring

--- src/main/java/org/apache/hbase/contrib/coprocessor/SecondaryIndexUpdater.java
+++ src/main/java/org/apache/hbase/contrib/coprocessor/SecondaryIndexUpdater.java
@@ -9,28 +9,38 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hbase.CoprocessorEnvironment;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
+import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
 import org.apache.hadoop.hbase.coprocessor.WALCoprocessorEnvironment;
 import org.apache.hadoop.hbase.coprocessor.WALObserver;
+import org.apache.hadoop.hbase.filter.FuzzyRowFilter;
 import org.apache.hadoop.hbase.regionserver.wal.HLogKey;
 import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.Pair;
 
 /**
- * Keeps the index table up to date when changes happen to the primary table.
- * It does that by monitoring the WAL writing.
+ * Keeps the index table up to date when changes happen to the primary table. It
+ * does that by monitoring the WAL writing.
  */
-public class SecondaryIndexUpdater implements WALObserver {
-	private static final Log LOG = LogFactory.getLog(SecondaryIndexUpdater.class);
+public class SecondaryIndexUpdater extends BaseRegionObserver implements
+		WALObserver {
+	private static final Log LOG = LogFactory
+			.getLog(SecondaryIndexUpdater.class);
 	private static final String PIMARY_TABLE_NAME = "tableName";
 	private static final String INDEX_TABLE_NAME = "idxTableName";
 	private static final String INDEXED_FAMILY = "indexedFamily";
 	private static final String INDEXED_QUALIFIER = "indexedQualifier";
 	private static final byte[] IDX_FAMILY = Bytes.toBytes("I");
 	private static final byte[] IDX_QUALIFIER = Bytes.toBytes("i");
-	
+
 	private byte[] primaryTableName;
 	private byte[] indexTableName;
 	private byte[] indexedFamily;
@@ -38,7 +48,8 @@ public class SecondaryIndexUpdater implements WALObserver {
 
 	@Override
 	public void start(CoprocessorEnvironment env) throws IOException {
-		this.primaryTableName = Bytes.toBytes(env.getConfiguration().get(PIMARY_TABLE_NAME));
+		this.primaryTableName = Bytes.toBytes(env.getConfiguration().get(
+				PIMARY_TABLE_NAME));
 		this.indexedFamily = Bytes.toBytes(env.getConfiguration().get(
 				INDEXED_FAMILY));
 		this.indexedQualifier = Bytes.toBytes(env.getConfiguration().get(
@@ -66,16 +77,60 @@ public class SecondaryIndexUpdater implements WALObserver {
 				if (Arrays.equals(family, this.indexedFamily)
 						&& Arrays.equals(qualifier, this.indexedQualifier)) {
 					LOG.debug("Found KeyValue from WALEdit which should be indexed.");
-					byte[] idxRow = createCompositeRowKey(kv.getValue(), kv.getRow());
-					if (kv.isDelete()) {
-						// remove from index
+					byte[] idxRow = createCompositeRowKey(kv.getValue(),
+							kv.getRow());
+					if (kv.isDelete() && kv.isDeleteType()) {
+						System.out
+								.println("delete cell ["
+										+ Bytes.toString(kv.getRow()) + " ,"
+										+ Bytes.toString(qualifier)
+										+ "] with version ["
+										+ kv.getTimestamp() + "]!");
+						HTableInterface idxTable = env.getEnvironment()
+								.getTable(indexTableName);
+						// partial row scan because first part is unknown
+						// ??_<state>
+						ResultScanner scanner = findRowsWithParialKey(idxTable,
+								kv.getRow());
+						Delete del = null;
+						for (Result res : scanner) {
+							// same timestamp?
+							if (res.raw()[0].getTimestamp() == kv
+									.getTimestamp()) {
+								del = new Delete(res.getRow(),
+										kv.getTimestamp(), null);
+								idxTable.delete(del);
+							}
+						}
+						scanner.close();
 					} else {
-						// update index
+						System.out.println("write row ["
+								+ Bytes.toString(idxRow) + "] with version ["
+								+ kv.getTimestamp() + "]!");
 						Put p = new Put(idxRow);
-						p.add(new KeyValue(idxRow, IDX_FAMILY, IDX_QUALIFIER, kv.getTimestamp(), Bytes.toBytes("")));
-						HTableInterface idxTable = env.getEnvironment().getTable(indexTableName);
+						p.add(IDX_FAMILY, IDX_QUALIFIER, kv.getTimestamp(),
+								Bytes.toBytes(""));
+						HTableInterface idxTable = env.getEnvironment()
+								.getTable(indexTableName);
 						idxTable.put(p);
+						idxTable.flushCommits();
 					}
+				} else if (kv.isDelete()
+						&& Arrays.equals(family, this.indexedFamily)
+						&& kv.isEmptyColumn()) {
+					System.out.println("delete entire row ["
+							+ Bytes.toString(kv.getRow()) + "]");
+					HTableInterface idxTable = env.getEnvironment().getTable(
+							indexTableName);
+					// partial row scan because first part is unknown ??_<state>
+					ResultScanner scanner = findRowsWithParialKey(idxTable,
+							kv.getRow());
+					Delete del = null;
+					for (Result res : scanner) {
+						del = new Delete(res.getRow());
+						idxTable.delete(del);
+					}
+					scanner.close();
 				}
 			}
 		}
@@ -90,8 +145,32 @@ public class SecondaryIndexUpdater implements WALObserver {
 	}
 
 	/**
-	 * Create composite row key by concatenating given byte[]. 
-	 * Delimiter between the byte[] is an underscore.
+	 * Triggered after {@link org.apache.hadoop.hbase.regionserver.HRegion} when
+	 * WAL is restored.
+	 */
+	@Override
+	public void postWALRestore(
+			ObserverContext<RegionCoprocessorEnvironment> env,
+			HRegionInfo info, HLogKey logKey, WALEdit logEdit)
+			throws IOException {
+	}
+
+	@SuppressWarnings("unchecked")
+	private ResultScanner findRowsWithParialKey(HTableInterface ht, byte[] row)
+			throws IOException {
+		FuzzyRowFilter rowFilter = new FuzzyRowFilter(
+				Arrays.asList(new Pair<byte[], byte[]>(createCompositeRowKey(
+						Bytes.toBytesBinary("\\x00\\x00"), row), new byte[] {
+						1, 1, 0, 0, 0 })));
+		Scan scan = new Scan();
+		scan.addColumn(IDX_FAMILY, IDX_QUALIFIER);
+		scan.setFilter(rowFilter);
+		return ht.getScanner(scan);
+	}
+
+	/**
+	 * Create composite row key by concatenating given byte[]. Delimiter between
+	 * the byte[] is an underscore.
 	 * 
 	 * @param columnValue
 	 * @param row
@@ -99,6 +178,19 @@ public class SecondaryIndexUpdater implements WALObserver {
 	 */
 	private byte[] createCompositeRowKey(byte[] columnValue, byte[] row) {
 		return Bytes.add(columnValue, Bytes.toBytes("_"), row);
+	}
+
+	private String printCellCoordinates(HTableInterface ht, byte[] family,
+			byte[] qualifier, long ts, byte[] value) {
+		StringBuilder buffer = new StringBuilder();
+		buffer.append(Bytes.toString(ht.getTableName()));
+		buffer.append(", ");
+		buffer.append(Bytes.toString(family));
+		buffer.append(", ");
+		buffer.append(Bytes.toString(qualifier));
+		buffer.append(", ");
+		buffer.append(ts);
+		return buffer.toString();
 	}
 
 }
--- src/test/java/org/apache/hbase/contrib/coprocessor/TestSecondaryIndexUpdater.java
+++ src/test/java/org/apache/hbase/contrib/coprocessor/TestSecondaryIndexUpdater.java
@@ -1,6 +1,7 @@
 package org.apache.hbase.contrib.coprocessor;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,9 +17,14 @@ import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Mutation;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.coprocessor.AggregationClient;
 import org.apache.hadoop.hbase.coprocessor.CoprocessorHost;
 import org.apache.hadoop.hbase.coprocessor.TestWALObserver;
 import org.apache.hadoop.hbase.coprocessor.WALObserver;
@@ -37,9 +43,14 @@ import org.junit.Test;
  */
 public class TestSecondaryIndexUpdater {
 	private static final HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility();
-	public static final byte[] USER_TABLE = Bytes.toBytes("user");
-	public static final byte[] USER_FAMILY_NAME = Bytes.toBytes("u");
 	public static final byte[] INDEX_TABLE = Bytes.toBytes("index");
+	public static final byte[] INDEX_FAMILY = Bytes.toBytes("I");
+	public static final byte[] INDEX_QUALIFIER = Bytes.toBytes("i");
+	public static final byte[] USER_TABLE = Bytes.toBytes("user");
+	private static byte[][] USER_FAMILY = { Bytes.toBytes("u"),
+			Bytes.toBytes("fam2"), Bytes.toBytes("fam3") };
+	private static byte[][] USER_QUALIFIER = { Bytes.toBytes("firstname"),
+			Bytes.toBytes("lastname"), Bytes.toBytes("state") };
 
 	private Configuration conf;
 	private FileSystem fs;
@@ -53,6 +64,8 @@ public class TestSecondaryIndexUpdater {
 		Configuration conf = TEST_UTIL.getConfiguration();
 		conf.set(CoprocessorHost.WAL_COPROCESSOR_CONF_KEY,
 				SecondaryIndexUpdater.class.getName());
+		conf.set(CoprocessorHost.REGION_COPROCESSOR_CONF_KEY,
+				"org.apache.hadoop.hbase.coprocessor.AggregateImplementation");
 		conf.setBoolean("dfs.support.append", true);
 		conf.set("tableName", "user");
 		conf.set("idxTableName", "index");
@@ -65,7 +78,7 @@ public class TestSecondaryIndexUpdater {
 		long upInNsec = System.nanoTime();
 		float delta = (float) (upInNsec - startInNsec) / 1000000000;
 		System.out.printf("Cluster up in %f seconds\n", delta);
-		
+
 		Path hbaseRootDir = TEST_UTIL.getDFSCluster().getFileSystem()
 				.makeQualified(new Path("/hbase"));
 		System.out.println("hbase.rootdir=" + hbaseRootDir);
@@ -94,7 +107,7 @@ public class TestSecondaryIndexUpdater {
 			TEST_UTIL.getDFSCluster().getFileSystem()
 					.delete(this.hbaseRootDir, true);
 		}
-
+		TEST_UTIL.truncateTable(INDEX_TABLE);
 	}
 
 	@After
@@ -104,51 +117,232 @@ public class TestSecondaryIndexUpdater {
 	}
 
 	@Test
-	public void testWriteToWALAndUpdateIndex() throws Exception {
+	public void testPutWithoutIndexedColumn() throws Throwable {
 		HRegionInfo hri = createHRegionInfo(Bytes.toString(USER_TABLE));
+		HLog log = createHLog(hri);
+		long[] stamps = makeTimestamps(1);
+		byte[] row = Bytes.toBytes("r1");
+		Put p = new Put(row);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[0], stamps[0], Bytes.toBytes("Hans"));
+		p.add(USER_FAMILY[0], USER_QUALIFIER[1], stamps[0], Bytes.toBytes("Muster"));
+		
+		writeToWAL(log, hri, p);
+		verifyTableSize(INDEX_TABLE, INDEX_FAMILY, INDEX_QUALIFIER, 0);
+	}
+
+	@Test
+	public void testPutWith3Versions() throws Throwable {
+		HRegionInfo hri = createHRegionInfo(Bytes.toString(USER_TABLE));
+		HLog log = createHLog(hri);
+
+		long[] stamps = makeTimestamps(3);
+		byte[][] values = { Bytes.toBytes("ZH"), Bytes.toBytes("BE"), Bytes.toBytes("TG") };
+		byte[] row = Bytes.toBytes("r2");
+		// insert 3 versions of same column
+		Put p = new Put(row);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[2], stamps[0], values[0]);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[2], stamps[1], values[1]);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[2], stamps[2], values[2]);
+
+		writeToWAL(log, hri, p);
+		getVersionAndVerify(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[0], values[0]);
+		getVersionAndVerify(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[1], values[1]);
+		getVersionAndVerify(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[2], values[2]);
+	}
+	
+	@Test
+	public void testDeleteEntireRow() throws Throwable {
+		HRegionInfo hri = createHRegionInfo(Bytes.toString(USER_TABLE));
+		HLog log = createHLog(hri);
+		
+		long[] stamps = makeTimestamps(3);
+		byte[][] values = { Bytes.toBytes("ZH"), Bytes.toBytes("BE"), Bytes.toBytes("TG") };
+		byte[] row = Bytes.toBytes("r3");
+		// insert 3 versions of same column
+		Put p = new Put(row);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[2], stamps[0], values[0]);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[2], stamps[1], values[1]);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[2], stamps[2], values[2]);
+
+		writeToWAL(log, hri, p);
+		// check versions on index table
+		getVersionAndVerify(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[0], values[0]);
+		getVersionAndVerify(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[1], values[1]);
+		getVersionAndVerify(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[2], values[2]);
+		
+		// delete row on main table
+		Delete del = new Delete(row);
+		del.deleteFamily(USER_FAMILY[0], stamps[2]+1);
+		writeToWAL(log, hri, del);
+		verifyTableSize(INDEX_TABLE, INDEX_FAMILY, INDEX_QUALIFIER, 0);
+	}
+	
+	@Test
+	public void testDeleteIndexedColumnVersion() throws Throwable {
+		HRegionInfo hri = createHRegionInfo(Bytes.toString(USER_TABLE));
+		HLog log = createHLog(hri);
+		
+		long[] stamps = makeTimestamps(3);
+		byte[][] values = { Bytes.toBytes("ZH"), Bytes.toBytes("BE"), Bytes.toBytes("TG") };
+		byte[] row = Bytes.toBytes("r4");
+		// insert 3 versions of same column
+		Put p = new Put(row);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[2], stamps[0], values[0]);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[2], stamps[1], values[1]);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[2], stamps[2], values[2]);
+		
+		writeToWAL(log, hri, p);
+		// check versions on index table
+		getVersionAndVerify(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[0], values[0]);
+		getVersionAndVerify(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[1], values[1]);
+		getVersionAndVerify(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[2], values[2]);
+		
+		// delete the specified version of the specified column in main table
+		Delete del = new Delete(row);
+		del.deleteColumn(USER_FAMILY[0], USER_QUALIFIER[2], stamps[1]);
+		writeToWAL(log, hri, del);
+		// check versions on index table
+		getVersionAndVerify(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[0], values[0]);
+		getVersionAndVerifyMissing(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[1], values[1]);
+		getVersionAndVerify(indexTable, row, INDEX_FAMILY, INDEX_QUALIFIER, stamps[2], values[2]);
+	}
+	
+	@Test
+	public void testDeleteNotIndexedColumnVersion() throws Throwable {
+		HRegionInfo hri = createHRegionInfo(Bytes.toString(USER_TABLE));
+		HLog log = createHLog(hri);
+		
+		long[] stamps = makeTimestamps(3);
+		byte[][] values = { Bytes.toBytes("Hans"), Bytes.toBytes("George"), Bytes.toBytes("Bill") };
+		byte[] row = Bytes.toBytes("r5");
+		// insert 3 versions of same column
+		Put p = new Put(row);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[0], stamps[0], values[0]);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[0], stamps[1], values[1]);
+		p.add(USER_FAMILY[0], USER_QUALIFIER[0], stamps[2], values[2]);
+		
+		writeToWAL(log, hri, p);
+		// check versions on index table
+		verifyTableSize(INDEX_TABLE, INDEX_FAMILY, INDEX_QUALIFIER, 0);
+		
+		// delete the specified version of the specified column in main table
+		Delete del = new Delete(row);
+		del.deleteColumn(USER_FAMILY[0], USER_QUALIFIER[0], stamps[2]);
+		writeToWAL(log, hri, del);
+		
+		// check versions on index table
+		verifyTableSize(INDEX_TABLE, INDEX_FAMILY, INDEX_QUALIFIER, 0);
+	}
+	
+	@Test
+	public void testWALReplay() throws Exception {
+		// TODO
+	}
+
+	private void getVersionAndVerifyMissing(HTable ht, byte[] row,
+			byte[] family, byte[] qualifier, long ts, byte[] value) throws Exception{
+		byte[] compRow = Bytes.add(value, Bytes.toBytes("_"), row);
+		Get get = new Get(compRow);
+		get.addColumn(family, qualifier);
+		get.setTimeStamp(ts);
+		Result result = ht.get(get);
+		assertEmptyResult(result, compRow, family, qualifier, value);
+	}
+
+	private void getVersionAndVerify(HTable ht, byte[] row, byte[] family,
+			byte[] qualifier, long ts, byte[] value) throws Exception {
+		byte[] compRow = Bytes.add(value, Bytes.toBytes("_"), row);
+		Get get = new Get(compRow);
+		get.addColumn(family, qualifier);
+		get.setTimeStamp(ts);
+		Result result = ht.get(get);
+		ht.get(get);
+		assertSingleResult(result, compRow, family, qualifier, value);
+	}
+	
+	private void assertEmptyResult(Result result, byte[] compRow,
+			byte[] family, byte[] qualifier, byte[] value) {
+		   assertTrue("expected an empty result but result contains " +
+			        result.size() + " keys", result.isEmpty());
+
+	}
+
+	private void assertSingleResult(Result result, byte[] row, byte[] family,
+			byte[] qualifier, byte[] value) throws Exception {
+		assertTrue("Expected row [" + Bytes.toString(row) + "] " + "Got row ["
+				+ Bytes.toString(result.getRow()) + "]",
+				equals(row, result.getRow()));
+	}
+
+	private boolean equals(byte[] left, byte[] right) {
+		if (left == null && right == null)
+			return true;
+		if (left == null && right.length == 0)
+			return true;
+		if (right == null && left.length == 0)
+			return true;
+		return Bytes.equals(left, right);
+	}
+
+	private void verifyTableSize(byte[] tablename, byte[] family, byte[] qualifier, long expected) throws Throwable {
+		AggregationClient ac = new AggregationClient(conf);
+		Scan scan = new Scan();
+		scan.addColumn(family, qualifier);
+		long rowCount = ac.rowCount(tablename, null, scan);
+		assertEquals(expected, rowCount);
+	}
+
+	private long[] makeTimestamps(int n) {
+		long[] stamps = new long[n];
+		for (int i = 0; i < n; i++) {
+			stamps[i] = i + 1;
+		}
+		return stamps;
+	}
+
+	private void writeToWAL(HLog log, HRegionInfo hri, Mutation... muts)
+			throws IOException {
+		Map<byte[], List<KeyValue>> familyMap = null;
+		WALEdit edit = new WALEdit();
+
+		for (Mutation mut : muts) {
+			familyMap = mut.getFamilyMap();
+			addFamilyMapToWALEdit(familyMap, edit);
+			familyMap.clear();
+		}
+
 		HTableDescriptor htd = createHTableDescriptor(Bytes
 				.toString(USER_TABLE));
-		Path basedir = new Path(this.hbaseRootDir, Bytes.toString(USER_TABLE));
-		deleteDir(basedir);
-		fs.mkdirs(new Path(basedir, hri.getEncodedName()));
-
-		HLog log = new HLog(this.fs, this.dir, oldLogDir, this.conf);
-
-		Put p = creatPutWith1Family(Bytes.toBytes("r1"));
-		Map<byte[], List<KeyValue>> familyMap = p.getFamilyMap();
-		WALEdit edit = new WALEdit();
-		addFamilyMapToWALEdit(familyMap, edit);
 
 		// it's the WAL write, the cp hook should occur
 		long now = EnvironmentEdgeManager.currentTimeMillis();
 		log.append(hri, hri.getTableName(), edit, now, htd);
-		
-		Get get = new Get(Bytes.toBytes("NY_r1"));
-		assertNotNull(indexTable.get(get).getRow());
+	}
+
+	private HLog createHLog(HRegionInfo hri) throws IOException {
+		Path basedir = new Path(this.hbaseRootDir, Bytes.toString(USER_TABLE));
+		deleteDir(basedir);
+		fs.mkdirs(new Path(basedir, hri.getEncodedName()));
+
+		return new HLog(this.fs, this.dir, oldLogDir, this.conf);
 	}
 
 	private HRegionInfo createHRegionInfo(final String tableName) {
 		HTableDescriptor htd = new HTableDescriptor(tableName);
-		HColumnDescriptor a = new HColumnDescriptor(USER_FAMILY_NAME);
-		htd.addFamily(a);
+		for (int i = 0; i < USER_FAMILY.length; i++) {
+			HColumnDescriptor a = new HColumnDescriptor(USER_FAMILY[i]);
+			htd.addFamily(a);
+		}
 		return new HRegionInfo(htd.getName(), null, null, false);
 	}
 
 	private HTableDescriptor createHTableDescriptor(final String tableName) {
 		HTableDescriptor htd = new HTableDescriptor(tableName);
-		HColumnDescriptor a = new HColumnDescriptor(Bytes.toBytes("a"));
-		htd.addFamily(a);
+		for (int i = 0; i < USER_FAMILY.length; i++) {
+			HColumnDescriptor a = new HColumnDescriptor(USER_FAMILY[i]);
+			htd.addFamily(a);
+		}
 		return htd;
-	}
-
-	private Put creatPutWith1Family(byte[] row) throws IOException {
-		Put p = new Put(row);
-		p.add(USER_FAMILY_NAME, Bytes.toBytes("firstname"),
-				Bytes.toBytes("Hans"));
-		p.add(USER_FAMILY_NAME, Bytes.toBytes("lastname"),
-				Bytes.toBytes("Muster"));
-		p.add(USER_FAMILY_NAME, Bytes.toBytes("state"), Bytes.toBytes("NY"));
-		return p;
 	}
 
 	/*
